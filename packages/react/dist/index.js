0c624f188462788e922600457ef14733d5ca3c63
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-10-20 22:59:49 -0300

    feat: log actions onClick

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -146,6 +146,7 @@ var Box = styled("div", {
   backgroundColor: "$gray800",
   border: "1px solid $gray600"
 });
+Box.displayName = "Box";
 
 // src/components/Text.tsx
 var Text = styled("p", {
@@ -174,6 +175,7 @@ var Text = styled("p", {
     size: "md"
   }
 });
+Text.displayName = "Text";
 
 // src/components/Heading.tsx
 var Heading = styled("h2", {
@@ -197,6 +199,7 @@ var Heading = styled("h2", {
     size: "md"
   }
 });
+Heading.displayName = "Heading";
 
 // src/components/Avatar/index.tsx
 var import_phosphor_react = require("phosphor-react");
@@ -245,6 +248,7 @@ function Avatar2(props) {
     ]
   });
 }
+Avatar2.displayName = "Avatar";
 
 // src/components/Button.tsx
 var Button = styled("button", {
@@ -317,6 +321,7 @@ var Button = styled("button", {
     size: "md"
   }
 });
+Button.displayName = "Button";
 
 // src/components/TextInput/styles.ts
 var TextInputContainer = styled("div", {
@@ -374,6 +379,7 @@ function TextInput({ prefix, ...props }) {
     ]
   });
 }
+TextInput.displayName = "TextInput";
 
 // src/components/TextArea.tsx
 var TextArea = styled("textarea", {
@@ -470,6 +476,7 @@ function Checkbox2(props) {
     })
   });
 }
+Checkbox2.displayName = "Checkbox";
 
 // src/components/MultiStep/styles.ts
 var MultiStepContainer = styled("div", {});
@@ -522,6 +529,7 @@ function MultiStep({ size, currentStep = 1 }) {
     ]
   });
 }
+MultiStep.displayName = "MultiStep";
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Avatar,
